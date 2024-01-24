ae51b2e0481331ede2bb5a7574bdea4ea41b86bf
Inna Antypova <nightallatiel@gmail.com>
2024-01-24 15:51:45 +0300

    регистрация моделей в админке

--- mail_sender/admin.py
+++ mail_sender/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from mail_sender.models import Client, MailingSettings, MailingMessage, MailingLog
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'full_name', 'comment')
+
+
+@admin.register(MailingSettings)
+class MailingSettingsAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'start_time', 'stop_time', 'periodicity', 'status')
+    list_filter = ('periodicity', 'status')
+
+
+@admin.register(MailingMessage)
+class MailingMessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'title', 'body')
+
+
+@admin.register(MailingLog)
+class MailingLogAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'last_try', 'status')
