45eaf493ae854f13250de3f9e3561a1b7ea2bea1
Inna Antypova <nightallatiel@gmail.com>
2024-02-02 18:22:26 +0300

    добавлен функционал Модератора

--- mail_sender/urls.py
+++ mail_sender/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from mail_sender.apps import MailSenderConfig
 from mail_sender.views import IndexView, ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, \
     ClientDeleteView, MailingSettingsListView, MailingSettingsDetailView, MailingSettingsCreateView, \
-    MailingSettingsUpdateView, MailingSettingsDeleteView, PersonalAreaView, MailingMessageCreateView
+    MailingSettingsUpdateView, MailingSettingsDeleteView, PersonalAreaView, MailingMessageCreateView, \
+    MailingMessageListView, MailingMessageUpdateView, MailingMessageDeleteView, MailingMessageDetailView, \
+    MailingLogListView
 
 app_name = MailSenderConfig.name
 
@@ -16,9 +18,14 @@ urlpatterns = [
     path('client_update/<int:pk>', ClientUpdateView.as_view(), name='client_update'),
     path('client_delete/<int:pk>', ClientDeleteView.as_view(), name='client_delete'),
     path('message_create/', MailingMessageCreateView.as_view(), name='create_message'),
+    path('message_list/', MailingMessageListView.as_view(), name='list_messages'),
+    path('message_edit/<int:pk>', MailingMessageUpdateView.as_view(), name='update_message'),
+    path('message_detail/<int:pk>', MailingMessageDetailView.as_view(), name='detail_message'),
+    path('message_delete/<int:pk>', MailingMessageDeleteView.as_view(), name='delete_message'),
     path('mails_list/', MailingSettingsListView.as_view(), name='mails_list'),
     path('mails_detail/<int:pk>', MailingSettingsDetailView.as_view(), name='mails_detail'),
     path('mails_create/', MailingSettingsCreateView.as_view(), name='mails_create'),
     path('mails_edit/<int:pk>', MailingSettingsUpdateView.as_view(), name='mails_edit'),
     path('mails_delete/<int:pk>', MailingSettingsDeleteView.as_view(), name='mails_delete'),
+    path('logs/', MailingLogListView.as_view(), name='logs'),
 ]
--- mail_sender/views.py
+++ mail_sender/views.py
@@ -1,26 +1,51 @@
-from urllib import request
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-from django.forms import inlineformset_factory
-
-from django.shortcuts import render
 from django.urls import reverse, reverse_lazy
 from django.views.generic import TemplateView, ListView, CreateView, UpdateView, DetailView, DeleteView
 
-from mail_sender.forms import MailingMessageForm, MailingSettingsForm, ClientForm
-from mail_sender.models import Client, MailingSettings, MailingMessage
+from mail_sender.forms import MailingMessageForm, MailingSettingsForm, ClientForm, ModeratorMailingSettingsForm
+from mail_sender.models import Client, MailingSettings, MailingMessage, MailingLog
+
+
+# class FormValidMixin(FormMixin):
+#     def form_valid(self, form):
+#         if form.is_valid():
+#             self.object = form.save()
+#             self.object.owners = self.request.user  # владелец
+#             self.object.save()
+#         return super().form_valid(form)
+#
+#     def form_valid_update(self, form):
+#         if self.object.owners == self.request.user:
+#             self.object.owners = self.request.user
+#             self.object.save()
+#         if form.is_valid():
+#             self.object = form.save()
+#             self.object.save()
+#         return super().form_valid(form)
 
 
 class IndexView(TemplateView):
+    """ Контроллер для главной страницы """
     template_name = 'mail_sender/index.html'
 
 
-class ClientListView(ListView):
+class ClientListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    """ Контроллер показывает клиентов пользователя """
     model = Client
+    permission_required = 'mail_sender.view_client'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(owners=self.request.user)
+        return queryset
 
 
-class ClientCreateView(CreateView):
+class ClientCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    """ Контроллер добавляет нового клиента пользователю """
     model = Client
     form_class = ClientForm
+    permission_required = 'mail_sender.add_client'
 
     def get_success_url(self):
         return reverse('mail_sender:client_list')
@@ -28,86 +53,189 @@ class ClientCreateView(CreateView):
     def form_valid(self, form):
         if form.is_valid():
             self.object = form.save()
+            self.object.owners = self.request.user  # владелец
             self.object.save()
         return super().form_valid(form)
 
 
-class ClientUpdateView(UpdateView):
+class ClientUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    """ Контроллер для изменения информации о клиенте """
     model = Client
     form_class = ClientForm
+    permission_required = 'mail_sender.change_client'
 
     def get_success_url(self):
         return reverse('mail_sender:client_list')
 
+    def get_object(self, *args, **kwargs):
+        client = super().get_object(*args, **kwargs)
+        if client.owners == self.request.user:  # проверка на владельца
+            return client
+        return reverse('mail_sender:client_list')
+
     def form_valid(self, form):
+        if self.object.owners == self.request.user:
+            self.object.owners = self.request.user
+            self.object.save()
         if form.is_valid():
             self.object = form.save()
             self.object.save()
         return super().form_valid(form)
 
 
-class ClientDetailView(DetailView):
+class ClientDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """ Контроллер для показа полной информации и клиенте """
     model = Client
+    permission_required = 'mail_sender.view_client'
 
 
-class ClientDeleteView(DeleteView):
+class ClientDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    """ Контроллер для удаления клиента """
     model = Client
     success_url = reverse_lazy('mail_sender:client_list')
+    permission_required = 'mail_sender.delete_client'
 
 
-class MailingSettingsListView(ListView):
+class MailingSettingsListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    """ Контроллера для показа списка рассылок пользователя """
     model = MailingSettings
+    permission_required = 'mail_sender.view_mailingsettings'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        if self.request.user.groups.filter(name='Moderator').exists() or self.request.user.is_superuser:
+            return queryset.all()
+        queryset = queryset.filter(owners=self.request.user)
+        return queryset
 
 
-class MailingSettingsDetailView(DetailView):
+class MailingSettingsDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """ Контроллер для показа детальной информации об рассылке """
     model = MailingSettings
+    permission_required = 'mail_sender.view_mailingsettings'
+
+    def get_context_data(self, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        clients = MailingSettings.objects.get(id=self.kwargs.get('pk')).client.all()  # клиенты пользователя
+        # print(clients)
+        context_data['clients'] = clients
+        return context_data
 
 
-class MailingSettingsCreateView(CreateView):
+class MailingSettingsCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    """ Контроллер для создания рассылки """
     model = MailingSettings
     form_class = MailingSettingsForm
+    permission_required = 'mail_sender.add_mailingsettings'
 
     def get_success_url(self):
         return reverse('mail_sender:mails_list')
 
-    # def get_context_data(self, **kwargs):
-    #     context_data = super().get_context_data(**kwargs)
-    #     MessageFormset = inlineformset_factory(Client, MailingMessage, form=MailingMessageForm)
-    #     if self.request.method == 'POST':
-    #         context_data['formset'] = MessageFormset(self.request.POST, instance=self.object, extra=1, max_num=1)
-    #     else:
-    #         context_data['formset'] = MessageFormset(instance=self.object)
-    #     return context_data
-    #
-    # def form_valid(self, form):
-    #     formset = self.get_context_data()['formset']
-    #     self.object = form.save()
-    #     if formset.is_valid():
-    #         formset.instance = self.object
-    #         formset.save()
-    #     return super().form_valid(form)
+    def form_valid(self, form):
+        if form.is_valid():
+            self.object = form.save()
+            self.object.owners = self.request.user
+            self.object.save()
+        return super().form_valid(form)
 
 
-class MailingSettingsUpdateView(UpdateView):
+class MailingSettingsUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    """ Контроллер для изменения рассылки """
     model = MailingSettings
-    form_class = MailingSettingsForm
+    permission_required = 'mail_sender.change_mailingsettings'
 
     def get_success_url(self):
         return reverse('mail_sender:mails_list')
 
+    def get_form_class(self):
+        if (self.request.user.has_perm('mail_sender.set_active') and self.request.user.is_staff) \
+                or self.request.user.is_superuser:
+            return ModeratorMailingSettingsForm
+        return MailingSettingsForm
 
-class MailingSettingsDeleteView(DeleteView):
+    def form_valid(self, form):
+        if self.object.owners == self.request.user:
+            self.object.owners = self.request.user
+            self.object.save()
+            if form.is_valid():
+                self.object = form.save()
+                self.object.save()
+        return super().form_valid(form)
+
+
+class MailingSettingsDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    """ Контроллер для удаления рассылки """
     model = MailingSettings
     success_url = reverse_lazy('mail_sender:mails_list')
+    permission_required = 'mail_sender.view_mailingsettings'
 
 
-class PersonalAreaView(TemplateView):
+class PersonalAreaView(LoginRequiredMixin, TemplateView):
+    """ Контроллер для отображения Личного кабинета """
     template_name = 'mail_sender/personal_area.html'
 
 
-class MailingMessageCreateView(CreateView):
+class MailingMessageCreateView(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+    """ Контроллера для создания сообщения для рассылки """
     model = MailingMessage
     form_class = MailingMessageForm
+    permission_required = 'mail_sender.add_mailingmessage'
 
     def get_success_url(self):
-        return reverse('mail_sender:mails_create')
+        return reverse('mail_sender/personal_area.html')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            self.object = form.save()
+            self.object.owners = self.request.user
+            self.object.save()
+            return super().form_valid(form)
+
+
+class MailingMessageUpdateView(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+    """ Контроллер для изменения сообщения для рассылки """
+    model = MailingMessage
+    form_class = MailingMessageForm
+    permission_required = 'mail_sender.change_mailingmessage'
+
+    def get_success_url(self):
+        return reverse('mail_sender:personal_area')
+
+    def form_valid(self, form):
+        if self.object.owners == self.request.user:
+            self.object.owners = self.request.user
+            self.object.save()
+        if form.is_valid():
+            self.object = form.save()
+            self.object.save()
+        return super().form_valid(form)
+
+
+class MailingMessageListView(LoginRequiredMixin, PermissionRequiredMixin, ListView):
+    """ Контроллер для показа списка сообщений """
+    model = MailingMessage
+    permission_required = 'mail_sender.view_mailingmessage'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        queryset = queryset.filter(owners=self.request.user)  # владелец
+        return queryset
+
+
+class MailingMessageDetailView(LoginRequiredMixin, PermissionRequiredMixin, DetailView):
+    """ Контроллер для показа детальной информации о сообщении """
+    model = MailingMessage
+    permission_required = 'mail_sender.view_mailingmessage'
+
+
+class MailingMessageDeleteView(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+    """ Контроллер для удаления сообщения """
+    model = MailingMessage
+    success_url = reverse_lazy('mail_sender:list_messages')
+    permission_required = 'mail_sender.delete_mailingmessage'
+
+
+class MailingLogListView(LoginRequiredMixin, ListView):
+    """ Контроллер для показа списка логов """
+    model = MailingLog
+
