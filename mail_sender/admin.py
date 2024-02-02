8f49de09ad5e3621a7fded857dd72ce68a211ce4
Inna Antypova <nightallatiel@gmail.com>
2024-02-02 18:19:59 +0300

    обновление отображений в админке

--- mail_sender/admin.py
+++ mail_sender/admin.py
@@ -5,20 +5,20 @@ from mail_sender.models import Client, MailingSettings, MailingMessage, MailingL
 
 @admin.register(Client)
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'email', 'full_name', 'comment')
+    list_display = ('pk', 'email', 'full_name', 'comment', 'owners')
 
 
 @admin.register(MailingSettings)
 class MailingSettingsAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'email', 'start_time', 'stop_time', 'periodicity', 'status')
+    list_display = ('pk', 'name', 'start_time', 'stop_time', 'periodicity', 'message', 'status', 'owners')
     list_filter = ('periodicity', 'status')
 
 
 @admin.register(MailingMessage)
 class MailingMessageAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'email', 'title', 'body')
+    list_display = ('pk', 'title', 'body', 'owners')
 
 
 @admin.register(MailingLog)
 class MailingLogAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'email', 'last_try', 'status')
+    list_display = ('pk', 'mailing_name', 'last_try', 'status', 'response')
